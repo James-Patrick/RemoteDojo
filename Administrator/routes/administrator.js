8de95a8ff03c0711fc58a1bffb247eb0f67e4728
paul <@>
2015-09-03 00:23:53 +0800

    See description.
    First commit.

---
+++ Administrator/routes/administrator.js
@@ -0,0 +1,25 @@
+﻿var dry_layers = require('dry-layers');
+
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', dry_layers.SecurityService.authenticated, function (req, res) {
+    var c1 = dry_layers.Registry.getCollection('dojo');
+    c1.find().toArray(function (err, dojos) {
+        if (err) {
+            res.status(500);
+            return;
+        }
+        var c2 = dry_layers.Registry.getCollection('role');
+        c2.find().toArray(function (err, roles) {
+            res.render('administrator', {
+                title: "Administrator",
+                dojos: dojos,
+                roles: roles
+            });
+        });
+    });
+});
+
+module.exports = router;
---
+++ Administrator/routes/index.js
@@ -0,0 +1,21 @@
+﻿var dry_layers = require('dry-layers');
+
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    if (null == dry_layers.Registry.getDatabase()) {
+        var err = dry_layers.Registry.getError();
+        if (err) {
+            res.render('error', {
+                message : 'Cannot connect to database',
+                error : err
+            });
+            return;
+        }
+    }
+    res.render('index', { title: 'CoderDojo' });
+});
+
+module.exports = router;
