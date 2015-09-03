40647e02eec0fdeddd61a7e1604d47f29914ff27
paul <@>
2015-09-03 11:06:35 +0800

    See description.
    Synced 'node_modules'.

--- Administrator/bin/www.js
+++ Administrator/bin/www.js
@@ -1,4 +1,6 @@
-﻿var debug = require('debug')('Administrator');
+﻿#!/usr/bin/env node
+
+var debug = require('debug')('Administrator');
 var mongo = require('mongodb');
 mongo.MongoClient.connect('mongodb://localhost:27017/coderdojo', function (err, db) {
 	var dry = require('dry-layers');
@@ -12,7 +14,7 @@ mongo.MongoClient.connect('mongodb://localhost:27017/coderdojo', function (err, 
 	}
 	dry.Registry.setDatabase(db);
 	var app = require('../app.js');
-	app.set('port', process.env.PORT || 8001);
+	app.set('port', process.env.PORT || 3000);
 	var server = app.listen(app.get('port'), function () {
 		debug('Express server listening on port ' + server.address().port);
 	});
