c528f38bd12a0798cddf0555e31bfc98cff4b4fb
Ben <Deeksie3000@gmail.com>
2015-09-22 12:53:59 +0800

    Superficial Modifications
    - Removed unused variable from and added a couple comments to
    WebRTC/bin/www
    - Removed unused route from communication server
    - Fixed layout.jade to stop complaining about being unable to find
    stylesheets we're not using
    - Added FavIcon (with transparency) #priorities

--- WebRTC/app.js
+++ WebRTC/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var mentor = require('./routes/mentor');
 var ninja = require('./routes/ninja');
 
@@ -17,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/Mentor',mentor);
 app.use('/Ninja',ninja);
 
