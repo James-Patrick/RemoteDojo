3d68627b8de38fef1ba280d844876fa3cc100f75
Ben <Deeksie3000@gmail.com>
2015-09-28 14:28:14 +0800

    Revert "Temporary patch for default administrator"
    This reverts commit 20217433902e7580c29c41bfa061456cce19dd1f.

--- defaultAdmin.js
+++
@@ -1,4 +0,0 @@
-use coderdojo
-var defaultAdmin = db.user.find({_id:'1'});
-if (defaultAdmin.length() == 0)
-	db.user.insert({_id : '1', 'username' : 'default', 'password' : 'secret', 'roles' : ['Administrator']});
